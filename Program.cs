a8de87d8541bef9e6cd589bb8f73b0a3b957f9e7
juliet_coder <jlsw06@yandex.ru>
2021-03-06 20:58:17 +0300

    c3

--- Program.cs
+++ Program.cs
@@ -5,88 +5,51 @@ using System.Linq;
 using System.Threading.Tasks;
 using YandexDisk.Client;
 using YandexDisk.Client.Http;
-using YandexDisk.Client.Protocol;
 
 namespace ConsoleApp
 {
-    class Program        //  C:\Users\Dom\Documents\Test
-    {                     // client/albums
-
+    internal class Program        // C:\Users\Dom\Desktop\Test
+    {                             // myFolderTest
         private static string token = "";
 
         public static async Task Main(string[] args)
         {
-            try
+            token = ConfigurationManager.AppSettings["oauthToken"];
+            var userDir = GetUserLocalFolder(@"Введите адрес локальной директории, например - C:\user\folder\");
+            var yaDiskDir = GetYandexDiskDirectory(@"Введите адрес папки на Яндекс Диске (при загрузке в корневой каталог - Enter)");
+            IDiskApi diskApi = new DiskHttpApi(token);
+
+            string[] files = Directory.GetFiles(userDir, "*");
+
+            foreach (var file in files)
             {
-                token = ConfigurationManager.AppSettings["oauthToken"];
-              var userDir = GetUserLocalFolder(@"Введите адрес локальной директории, например - C:\user\folder\");   
-                var yaDiskDir = GetYandexDiskDirectory(@"Введите адрес папки на Яндекс Диске (при загрузке в корневой каталог - Enter)");
-                IDiskApi diskApi = new DiskHttpApi(token);
-
-                var files = userDir.Length;
                 
-                if (!userDir.EndsWith("" + Path.DirectorySeparatorChar)) files++;
-
-                var filesPath = Directory
-                                .EnumerateFiles(userDir, "*", SearchOption.AllDirectories)
-                                .Select(f => f.Substring(files));
-                foreach (var file in filesPath)
+                try
                 {
-                    await GetStartAsync(yaDiskDir, diskApi, file);
+                    file.Remove(0, file.LastIndexOf('\\') + 1);
+                    Console.WriteLine(file, "Загрузка");
+                    var url = await diskApi.Files.GetUploadLinkAsync("/" + yaDiskDir + "/" + file, overwrite: false);
+                    using var fs = File.OpenRead(file);
+                    await diskApi.Files.UploadAsync(url, fs);
+                    Console.WriteLine(file, "Загружен");
                 }
-
+                catch (Exception e)
+                {
+                    Console.WriteLine("Ошибка загрузки файла " + e.Message);
+                }
             }
-
-
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-        }
-        
-        public static async Task GetStartAsync(string yaDiskDir, IDiskApi diskApi, string file)
-        {           
-            Link url = await diskApi.Files.GetUploadLinkAsync(yaDiskDir + "/" + file, true)
-            .ConfigureAwait(false);
-
-            Console.WriteLine(file, "Загрузка");
-
-            using (FileStream fs = File.OpenRead(file))
-            {
-                
-                await diskApi.Files.UploadAsync(url, fs);
-            }
-
-            Console.WriteLine(file, "Загружен");
         }
 
         private static string GetUserLocalFolder(string inputInternal)
-                {
-                    Console.Write(inputInternal);
-                    return Console.ReadLine();
-                }
-
-                public static string GetYandexDiskDirectory(string InputExternal)
-                {
-                    Console.Write(InputExternal);
-                    return Console.ReadLine();
-                }
-
-       /* private static void GetUserInput(out UploadParam uploadParam)
         {
-            Console.WriteLine(@"Введите адрес локальной директории, например - C:\user\folder\");
-            string userDir = Console.ReadLine();
+            Console.Write(inputInternal);
+            return Console.ReadLine();
+        }
 
-            Console.WriteLine("Введите адрес папки на Яндекс Диске (при загрузке в корневой каталог - Enter)");
-            string yaDiskDir = Console.ReadLine();
-
-            uploadParam = new UploadParam(userDir, yaDiskDir);
-        } */
+        public static string GetYandexDiskDirectory(string InputExternal)
+        {
+            Console.Write(InputExternal);
+            return Console.ReadLine();
+        }
     }
 }
-
-
-
-
-
-
